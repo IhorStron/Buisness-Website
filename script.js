bfd5e8e234182615cb9a16f5df98f161220d9181
IhorStron <worklife0025@gmail.com>
2024-03-26 18:33:56 +0200

    added section 2

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ menu.addEventListener('click', () => {
 })
 
 // End of Navbar
+
+// Section 2 Video 
+
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons button i')
+const bar = document.querySelector('.video__bar')
+
+const palyPause = () => {
+   if (video.paused) {
+      video.play()
+      btn.className = 'far fa-pause-circle'
+      video.style.opacity = '0.7'
+   } else {
+      video.pause()
+      btn.className = 'far fa-play-circle'
+      video.style.opacity = '0.3'
+   }
+
+}
+
+btn.addEventListener('click', () => {
+   palyPause()
+})
+
+video.addEventListener('timeupdate', () => {
+   console.log(video.currentTime, video.duration)
+   const barWidth = video.currentTime / video.duration
+   bar.style.width = `${barWidth * 100}%`
+   if(video.ended) {
+      btn.className = 'far fa-play-circle'
+      video.style.opacity = '0.3'
+    } 
+})
+
+// End of Section 2 Video 
